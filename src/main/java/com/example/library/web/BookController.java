7c48aa05438779851b8c9dc8d695175eaa721691
mabi0110 <biziewskamk@gmail.com>
2024-02-07 00:08:55 +0100

    add 404-page implementation

--- src/main/java/com/example/library/web/BookController.java
+++ src/main/java/com/example/library/web/BookController.java
@@ -2,10 +2,12 @@ package com.example.library.web;
 
 import com.example.library.domain.book.BookService;
 import com.example.library.domain.book.dto.BookDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -20,8 +22,9 @@ public class BookController {
 
     @GetMapping("/book/{id}")
     public String getBook(@PathVariable long id, Model model) {
-        Optional<BookDto> optionalBook = bookService.findBookById(id);
-        optionalBook.ifPresent(book -> model.addAttribute("book", book));
+        BookDto book = bookService.findBookById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        model.addAttribute("book", book);
         return "book";
 
     }
